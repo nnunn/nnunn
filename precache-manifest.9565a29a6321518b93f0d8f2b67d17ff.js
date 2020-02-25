f9b6d981a51b7e40623983417e227191999982f8
nnunn <natasha@nnunn.ca>
2020-02-25 14:46:20 -0700

    Updates

---
+++ precache-manifest.9565a29a6321518b93f0d8f2b67d17ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "045456e9689d63acf41a3f986dcc8d73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e764451050533d245907",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "811fdf4aa4ac59301843",
+    "url": "/static/js/2.9c0840b9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.9c0840b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e764451050533d245907",
+    "url": "/static/js/main.cdfe9401.chunk.js"
+  },
+  {
+    "revision": "a1516129bc646f86b88c",
+    "url": "/static/js/runtime-main.9e39bbed.js"
+  }
+]);
